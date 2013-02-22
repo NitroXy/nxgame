98a100eace63d91866584a9cbdd08262e63f4b05
Sebastian <sebbe@kvarnaberget.se>
2013-02-22 19:17:04 +0100

    Ankan is the answer ..

--- view/info/index.php
+++ view/info/index.php
@@ -1,1 +1,5 @@
 <p> Lite text här med ? </p>
+
+<img width="400" src="https://trello-attachments.s3.amazonaws.com/5123e0c21b92132360004804/5123e36b036058ed1a004c69/d6ddb3af8480f04d47f9fe6ac9207d8b/Untitled.png">
+
+</br>
