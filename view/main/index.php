336d3eb72ca05ca385897b7d7f1d896e9d4109d9
cpluss <sebastian@cpluss.se>
2013-02-27 21:15:43 +0100

    Added correct formatting at mainpage, converted h1 to h2. Corrected style for h2

--- view/main/index.php
+++ view/main/index.php
@@ -1,18 +1,11 @@
-<view>
-	<content>
-		<h1>NXGame 16</h1>
-		<p>Välkommen in i värmen till det som i begynnelsen refererades till som "?main=nxgame", gömd från resten av nitroxys extremt underbara hemsida och verklighet. Nu, ett helt millenium (+-1000 år) senare, har nxgame återupptagits, med nya troll och mindre kaffedrickare. Har du vad som krävs för att utmana dig själv och alla fiender du kommer att stöta på?</p>
+<h2>NXGame 16</h2>
+<p>Välkommen in i värmen till det som i begynnelsen refererades till som "?main=nxgame", gömd från resten av nitroxys extremt underbara hemsida och verklighet. Nu, ett helt millenium (+-1000 år) senare, har nxgame återupptagits, med nya troll och mindre kaffedrickare. Har du vad som krävs för att utmana dig själv och alla fiender du kommer att stöta på?</p>
 
-		<p>NXGame är en rolig och underhållande <a href="http://en.wikipedia.org/wiki/Puzzle_hunt">problemlösartävling</a>, där vem som helst kan vara med.</p>
-		<p>Man behöver inga förkunskaper för att klara av spelet. Det enda man egentligen behöver är ett kreativt och logiskt tänkande för att lösa problemen man kommer utsättas för. Vem som helst kan bli en vinnare.</p>
-		<p>Vinnaren av NXGame kommer att vinna ett pris på en extremt <!--jävligt minimal och inte ens i närheten av en-->stor summa pengar. Vinnaren kan bli du! Titta bara på det här glada trollet som redan nx6 vann hela nxgame.</p>
-		<img src="images/joxx.jpg">	
-		<p> Visst vill du vara lika lycklig som denne herre? I så fall: Spela NXGame!</p>
-		<br>
-		<br>
-		<p>Och kom ihåg: Ankan är svaret.</p>
-
-
-	</content>
-<view>
-
+<p>NXGame är en rolig och underhållande <a href="http://en.wikipedia.org/wiki/Puzzle_hunt">problemlösartävling</a>, där vem som helst kan vara med.</p>
+<p>Man behöver inga förkunskaper för att klara av spelet. Det enda man egentligen behöver är ett kreativt och logiskt tänkande för att lösa problemen man kommer utsättas för. Vem som helst kan bli en vinnare.</p>
+<p>Vinnaren av NXGame kommer att vinna ett pris på en extremt <!--jävligt minimal och inte ens i närheten av en-->stor summa pengar. Vinnaren kan bli du! Titta bara på det här glada trollet som redan nx6 vann hela nxgame.</p>
+<img src="images/joxx.jpg">	
+<p> Visst vill du vara lika lycklig som denne herre? I så fall: Spela NXGame!</p>
+<br>
+<br>
+<p>Och kom ihåg: Ankan är svaret.</p>
