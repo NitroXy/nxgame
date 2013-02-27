6e8348175239cca067fe0d0b9df86851dff6aff0
Anklol <andreas.hagesjo@gmail.com>
2013-02-27 05:08:44 +0100

    updated content

--- view/main/index.php
+++ view/main/index.php
@@ -5,8 +5,11 @@
 
 		<p>NXGame är en rolig och underhållande <a href="http://en.wikipedia.org/wiki/Puzzle_hunt">problemlösartävling</a>, där vem som helst kan vara med.</p>
 		<p>Man behöver inga förkunskaper för att klara av spelet. Det enda man egentligen behöver är ett kreativt och logiskt tänkande för att lösa problemen man kommer utsättas för. Vem som helst kan bli en vinnare.</p>
-		<p>Vinnaren av NXGame kommer att vinna ett pris på en extremt <!--jävligt minimal och inte ens i närheten av en-->stor summa pengar.</p>
-		
+		<p>Vinnaren av NXGame kommer att vinna ett pris på en extremt <!--jävligt minimal och inte ens i närheten av en-->stor summa pengar. Vinnaren kan bli du! Titta bara på det här glada trollet som redan nx6 vann hela nxgame.</p>
+		<img src="images/joxx.jpg">	
+		<p> Visst vill du vara lika lycklig som denne herre? I så fall: Spela NXGame!</p>
+		<br>
+		<br>
 		<p>Och kom ihåg: Ankan är svaret.</p>
 
 
