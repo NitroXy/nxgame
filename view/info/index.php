7b3d10df3c7badb09a98f9d46ecea62e1a1beaf4
Anklol <andreas.hagesjo@gmail.com>
2013-02-27 23:09:30 +0100

    updated content

--- view/info/index.php
+++ view/info/index.php
@@ -1,91 +1,40 @@
-<h2>Rules &amp; General info</h2>
-<p></p>
-<ul>
-    <li>You win The DreamHack Game by answering questions as fast as possible - Not by cheating!</li>
-    <li>It is not allowed to tear down clues, try to hack the server or access any restricted areas.</li>
-    <li>You are encouraged compete in teams, but please try to keep the team size to a maximum of 10 people.</li>
-    <li>You must have a seat ticket at DreamHack to be able to win</li>
-    <li>All passwords are case-insensitive, it doesn't matter if you have capital letters or not!</li>
-    <li>The passwords are never completely random letters and numbers, but can be a combination of words and numbers or hexadecimal and binary digits.</li>
-    <li>The most important rule is that you are not allowed to ruin for anyone else. Leave visited places as they were when you got there, don't destroy clues.</li>
-    <li>Never enter into restricted areas, you will never need to enter an area not open to the public.</li>
-    <li>Do not post tasks, answers or clues in the IRC channel #dhgame @ quakenet</li>
-    <li>If you seem to have a problem of a technical nature, please contact the DreamHack Game crew. You can find us in #dhgame @ quakenet.</li>
-</ul>
+<view>
+<content>
+<h2>Regler och information</h2>
+<p>Du vinner NXGame genom att vara den som först svarar rätt på alla frågorna.
+<br>Det är inte tillåtet att på något sätt förstöra ledtrådar, t.ex riva ner lappar och ta bort saker som tillhör nxgame. Nxgame är till för att ha kul genom att tävla mot andra deltagare, och inte förstöra för dem. Skulle du misstänka att det är något som någon har rivit ned, tagit bort, eller förstört, gå till info/support och be att få prata med nxgame-crew. Gillar du inte människor så går det även bra att kontakta oss på irc, #nitroxy.nxgame @ EFNet
+<br> Du måste vara på plats i Tyngelvi, med en platsbiljett, för att kunna vinna.
+<br>Alla lösenord är inte case-sensitive, dvs om du fyllt i ett lösenords-ord där ordet är korrekt, men du har mixat stora och små bokstäver, så gör det inget, det accepteras ändå.
+<br>Lösenorden är aldrig helt slumpade siffror och bokstäver</p>
 
-<h2>
-	Help</h2>
-<p>
-	There is a clue hotline on skype if you call skype contact &quot;dhg.clueline&quot;. It is an answering machine to which you describe which episode and task you&#39;re on and what you have done so far. Be detailed but motivate why you have taken each step. You may speak Swedish. Try calling even if it looks like it is offline. If your reasoning is sound we will reward the call with a clue on the question, on the web page of the question.<br />
-	&nbsp;</p>
-<p>
-	The DreamHack game can be difficult and frustrating at times. This is part of the game. The ultimate goal of the DreamHack Game challenge is to be calm and think simple while trying to be the fastest thinker out there. It&#39;s often not as hard as you think it is. Head over to <strong>#dhgame @ quakenet</strong> on IRC to hang out with the creators and other players.</p>
-<h3>
-	For beginners</h3>
-<p>
-	Welcome to the exciting world of The DreamHack Game!</p>
-<p>
-	The game is all about figuring out what to do. As you play more you will get more and more into the spirit of the game and you will figure things out faster. It is important to play in a team or you&#39;ll have a difficult task finishing the game.</p>
-<p>
-	If you have general questions about the game, we in the DHGame-crew are happy to help. Just talk to us in <strong>#dhgame @ quakenet</strong>, or if you manage to find us in person :) The IRC channel is also very helpful for finding people to play with or to talk generally about the game. <em>However you must NOT post the tasks, clues or answers in the channel #dhgame</em>.</p>
-<p>
-	Good things to think about when trying to solve the game is numbers (count stuff), patterns and odd things that don&#39;t fit in. Tasks can be computer-related, but don&#39;t have to be, many times you will have to leave your seat and run to find clues. You might even have to interact with people, software or machines to be able to solve a task.</p>
-<h3>
-	For initiated players</h3>
-<p>
-	<strong>Think simple!</strong></p>
-<p>
-	Here&#39;s a list of things you might want to try!</p>
-<ol>
-	<li>
-		Google it!</li>
-	<li>
-		Take a walk to see if you can find something at DreamHack which relates to the task</li>
-	<li>
-		Convert it to HEX</li>
-	<li>
-		Convert it to Binary</li>
-	<li>
-		Convert it to Decimal</li>
-	<li>
-		Braille</li>
-	<li>
-		Morse code</li>
-	<li>
-		Alphabetize</li>
-	<li>
-		Rearrange the letters</li>
-	<li>
-		Look at the task in a mirror</li>
-	<li>
-		Say it out loud</li>
-	<li>
-		Make it into 3D</li>
-	<li>
-		Fold it</li>
-	<li>
-		Connect the dots</li>
-	<li>
-		Overlay it on a map of something</li>
-	<li>
-		Look at letter frequencies</li>
-	<li>
-		Determine if it&#39;s a cipher of some kind</li>
-	<li>
-		Shift from letters to numbers</li>
-	<li>
-		See if text relates to song lyrics or poems</li>
-	<li>
-		Look at a computer keyboard to match up patterns</li>
-	<li>
-		Graph numbers or letter frequencies</li>
-	<li>
-		Change base of numbers</li>
-	<li>
-		Treat numbers as phone numbers, IP address, dates, atomic weights, coordinates, radio stations, ASCII, ISBN</li>
-	<li>
-		Prime factor numbers</li>
-</ol>
-<p>
-	<br />
-	If nothing else works, leave it, go sleep and eat, then come back with new eyes.</p>
+
+<h2>Hjälp</h2>
+<p>Ibland kan du bli frustrerad över att du är totalt fast på en fråga och inte kommer någon vart. Ge inte upp! Oftast brukar man övertänka uppgiften man har framför sig, och försöka med jättekomplicerade och extremt långsökta lösningar. Lösningen på uppgiften brukar oftast vara enklare än vad du tror. Känner du att du fortfarande inte kommer någon vart när du verkligen har försökt med allt du kan komma på, kom då in på irc #nitroxy.nxgame och prata med skaparna av spelet. Är vi snälla så kanske vi då lägger ut extra ledtrådar till uppgiften på sidan. Allmäna frågor om spelet är också varmt välkomna. Om du inte känner dig bekväm med irc, så kan du alltid gå till info/support och fråga efter nxgame-crew.</p>
+
+
+<h2> För nybörjare</h2>
+<p>Först och främst handlar det om att komma in i tänket för nxgame; att komma in i hur man ska göra, och hitta ett flyt i hur man löser problemen. Övning ger färdighet!
+<br>En sak som är bra att tänka på är nummer. Räkna saker. Leta efter mönster, leta efter saker som inte passar in, saker som du tror står ut gentemot de andra sakerna du har framför dig. Uppgifterna kan vara dator-relaterade, men det behöver de inte vara. Oftast betyder att lösa en uppgift att du faktiskt behöver röra dig bort från din plats för att hitta ledtrådar. Du kan hitta lappar, elektroniska apparater och dylikt som du måste läsa av eller göra något speciellt med för att få ut ledtrådar för att klara av en uppgift.</p>
+
+<h2>Lista över saker att testa för att klara uppgiften:</h2>
+
+<ul>
+	<li> Googla!</li>
+	<li> Gå runt och se om du kan hitta något som relaterar till uppgiften du håller på med</li>
+	<li>Konvertera till HEX</li>
+	<li>Konvertera till Binärt</li>
+	<li>Konvertera till Base64</li>
+	<li>Morse kod</li>
+	<li>Kan det vara ett anagram?</li>
+	<li>Spegelvänd det!</li>
+	<li>Prick till prick</li>
+	<li>Säg det högt!</li>
+	<li>Är det ett chiffer?</li>
+	<li>Tolka siffror som ASCII-kod, datum, koordinater, telefonnummer med mera</li>
+	<li>Om inget annat fungerar, ta en paus, tänk på annat, gå och ät en stund, häng lite i offlan, prata lite med trevliga deltagare och crew, sov, och kom sedan tillbaka med nya tankar och förslag till hur du ska lösa uppgiften.</li>
+</ul>
+<br>
+<br>
+<h3> Lycka till, och må den finurligaste deltagaren vinna! - NXGame-crew.</h3>
+</content>
+</view>
