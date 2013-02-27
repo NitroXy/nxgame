b1fe6b6a1545599049417f82eccc52e7d9a977ca
cpluss <sebastian@cpluss.se>
2013-02-27 16:25:19 +0100

    Added a style test file at /test

---
+++ view/test/index.php
@@ -0,0 +1,10 @@
+<p class="error"> ETT ERROR FLASH MEDDELANDE!! </p>
+<p class="success"> ETT SUCCESS FLASH MEDDELANDE!! </p>
+<p class="info"> ETT INFO FLASH MEDDELANDE!! </p>
+
+<form>
+	form text! 
+</form>
+
+<p> En helt vanlig paragraf. </p>
+<span> SPAN </span>
