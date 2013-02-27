24ac219ce2e2c20df982755d8e81e990da67d344
Anklol <andreas.hagesjo@gmail.com>
2013-02-27 23:39:16 +0100

    Updated content and made the view better

--- view/info/index.php
+++ view/info/index.php
@@ -3,38 +3,40 @@
 <h2>Regler och information</h2>
 <p>Du vinner NXGame genom att vara den som först svarar rätt på alla frågorna.</p>
 <p>Det är inte tillåtet att på något sätt förstöra ledtrådar, t.ex riva ner lappar och ta bort saker som tillhör nxgame. Nxgame är till för att ha kul genom att tävla mot andra deltagare, och inte förstöra för dem. Skulle du misstänka att det är något som någon har rivit ned, tagit bort, eller förstört, gå till info/support och be att få prata med nxgame-crew. Gillar du inte människor så går det även bra att kontakta oss på irc, <strong>#nitroxy.nxgame @ EFNet.</strong></p>
-<p>Du måste vara på plats i Tyngelvi, med en platsbiljett, för att kunna vinna.</p>
+<p>Du måste vara på plats i Tyngelvi (dvs NitroXy), med en platsbiljett, för att kunna vinna.</p>
 <p>Alla lösenord är inte case-sensitive, dvs om du fyllt i ett lösenords-ord där ordet är korrekt, men du har mixat stora och små bokstäver, så gör det inget, det accepteras ändå.</p>
 <p>Lösenorden är aldrig helt slumpade siffror och bokstäver.</p>
-
+<br>
 
 <h2>Hjälp</h2>
 <p>Ibland kan du bli frustrerad över att du är totalt fast på en fråga och inte kommer någon vart. Ge inte upp! Oftast brukar man övertänka uppgiften man har framför sig, och försöka med jättekomplicerade och extremt långsökta lösningar. Lösningen på uppgiften brukar oftast vara enklare än vad du tror.</p>
 <p>Känner du att du fortfarande inte kommer någon vart när du verkligen har försökt med allt du kan komma på, kom då in på irc #nitroxy.nxgame och prata med skaparna av spelet. Är vi snälla så kanske vi då lägger ut extra ledtrådar till uppgiften på sidan. Allmäna frågor om spelet är också varmt välkomna. </p>
 <p>Om du inte känner dig bekväm med irc, så kan du alltid gå till info/support och fråga efter nxgame-crew.</p>
 
-
+<br>
 <h2> För nybörjare</h2>
 <p>Först och främst handlar det om att komma in i tänket för nxgame; att komma in i hur man ska göra, och hitta ett flyt i hur man löser uppgifterna. Övning ger färdighet!</p>
 <p>En sak som är bra att tänka på är nummer. Räkna saker. Leta efter mönster, leta efter saker som inte passar in, saker som du tror står ut gentemot de andra sakerna du har framför dig. Uppgifterna kan vara dator-relaterade, men det behöver de inte vara.</p>
 <p>Oftast betyder att lösa en uppgift att du faktiskt behöver röra dig bort från din plats för att hitta ledtrådar. Du kan hitta lappar, elektroniska apparater och dylikt som du måste läsa av eller göra något speciellt med för att få ut ledtrådar för att klara av en uppgift.</p>
-
-<h2>Lista över saker att testa för att klara uppgiften:</h2>
+<br>
+<h2>Generella saker man kan testa om man har fastnat:</h2>
 
 <ul>
-	<li>*  Googla!</li>
+	<li>*  Googla</li>
 	<li>*  Gå runt och se om du kan hitta något som relaterar till uppgiften du håller på med</li>
 	<li>* Konvertera till HEX</li>
 	<li>* Konvertera till Binärt</li>
 	<li>* Konvertera till Base64</li>
-	<li>* Morse kod</li>
+	<li>* Morsekod</li>
 	<li>* Kan det vara ett anagram?</li>
-	<li>* Spegelvänd det!</li>
+	<li>* Spegelvänd det</li>
 	<li>* Prick till prick</li>
-	<li>* Säg det högt!</li>
+	<li>* Säg det högt</li>
 	<li>* Är det ett chiffer?</li>
 	<li>* Tolka siffror som ASCII-kod, datum, koordinater, telefonnummer med mera</li>
-	<li>* Om inget annat fungerar, ta en paus, tänk på annat, gå och ät en stund, häng lite i offlan, prata lite med trevliga deltagare och crew, sov, och kom sedan tillbaka med nya tankar och förslag till hur du ska lösa uppgiften.</li>
+	<li>* Mystiska trådlösa anslutningar, såsom WLAN och bluetooth
+	<li>* En bild kan ibland vara mer än en bild
+	<li>* Om du känner att inget som du kan komma på för att lösa problemet fungerar: ta en paus, tänk på annat, gå och ät en stund, häng lite i offlan, prata lite med trevliga deltagare och crew, sov, och kom sedan tillbaka med nya tankar och förslag till hur du ska lösa uppgiften</li>
 </ul>
 <br>
 <br>
