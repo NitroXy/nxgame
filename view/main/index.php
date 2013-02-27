f9b4b23cc0bc2bbbe6d2901f03be9b03713ac42f
Anklol <andreas.hagesjo@gmail.com>
2013-02-27 03:41:53 +0100

    Updated content

--- view/main/index.php
+++ view/main/index.php
@@ -1,1 +1,15 @@
-<p> Lite text här kanske? </p>
+<view>
+	<content>
+		<center><h1><u>NXGame 16</u></h1></center>
+		<p>Välkommen in i värmen till det som i begynnelsen refererades till som "?main=nxgame", gömd från resten av nitroxys extremt underbara hemsida och verklighet. Nu, ett helt millenium (+-1000 år) senare, har nxgame återupptagits, med nya troll och mindre kaffedrickare. Har du vad som krävs för att utmana dig själv och alla fiender du kommer att stöta på?</p>
+
+		<p>NXGame är en rolig och underhållande <a href="http://en.wikipedia.org/wiki/Puzzle_hunt">problemlösartävling</a>, där vem som helst kan vara med.</p>
+		<p>Man behöver inga förkunskaper för att klara av spelet. Det enda man egentligen behöver är ett kreativt och logiskt tänkande för att lösa problemen man kommer utsättas för. Vem som helst kan bli en vinnare.</p>
+		<p>Vinnaren av NXGame kommer att vinna ett pris på en extremt <!--jävligt minimal och inte ens i närheten av en-->stor summa pengar.</p>
+		
+		<p>Och kom ihåg: Ankan är svaret.</p>
+
+
+	</content>
+<view>
+
