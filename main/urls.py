8fab86f7863911a19847cab0c0125c645c18dff4
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-05-19 23:27:41 +0200

    Added views/urls for the new sub-pages

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
 from . import views
+from views import *
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+        url(r'^rules/$', views.rules, name='rules'),
+        url(r'^tutorial/$', views.tutorial, name='tutorial'),
+	url(r'^game/(?P<curr_game>[0-9]*)/?$', views.game, name='game'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,37 @@
 from os import listdir, path
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.conf import settings
+from .models import *
 
-# Create your views here.
+def game(request, curr_game):
+    # temporary
+    active_games = Game.objects.filter(episode__is_active=True).distinct()
+    return render(request, 'main/game.html', {'game' : curr_game, 'active_games' : active_games })
+
 def index(request):
-	print request.session.get_expiry_date()
-	if request.user.is_authenticated(): # temporary
-		return render(request, 'main/auth.html')
-	else:
-		return render(request, 'main/index.html')
+    if request.user.is_authenticated(): # temporary
+        print User_episode.objects.filter(user=request.user)
+        return render(request, 'main/auth.html')
+    else:
+        return render(request, 'main/index.html')
+
+def tutorial(request):
+    return render(request, 'main/tutorial.html')
+
+def rules(request):
+    return render(request, 'main/rules.html')
+
+
+# This might not belong here. But yeah, keep it here at the moment.
+def check_auth(request):
+    """Instead of using django's admin-interface, redirect to CAS-login if user tries to access
+    the admin site while not being authenticated"""
+    if request.user.is_authenticated():
+        if request.user.is_superuser:
+            return redirect('/admin/')
+        else:
+            return render(request, 'main/index.html')
+    else:
+        return redirect('/login/')
